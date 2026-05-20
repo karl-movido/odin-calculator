66c5018444e467db671f557969f45b5b73df3b31
Karl Movido <karl.movido@gmail.com>
2026-05-20 21:31:58 +0800

    feat: added buttons and operate function

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ const num1 = 0;
 const num2 = 0;
 const op = '';
 
+function operate(a, b, op) {
+    switch (op) {
+        case '+':
+           return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            return;
+            break;
+    }
+}
