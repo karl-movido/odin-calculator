d01a0f13547df8b28e0000b3cc6eb19eb2a2c4f0
Karl Movido <karl.movido@gmail.com>
2026-05-20 21:22:02 +0800

    feat: added basic operations

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const num1 = 0;
+const num2 = 0;
+const op = '';
+
