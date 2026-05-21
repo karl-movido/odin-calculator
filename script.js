eddebc93442cccd8fc28e23a10b699d2d3fe83cb
Karl Movido <karl.movido@gmail.com>
2026-05-21 19:10:21 +0800

    feat: added style

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    return a / b;
+  return a / b;
 }
 
 const num1 = 0;
 const num2 = 0;
-const op = '';
+const op = "";
 
 function operate(a, b, op) {
-    switch (op) {
-        case '+':
-           return add(a, b);
-            break;
-        case '-':
-            return subtract(a, b);
-            break;
-        case '*':
-            return multiply(a, b);
-            break;
-        case '/':
-            return divide(a, b);
-            break;
-        default:
-            return;
-            break;
-    }
+  switch (op) {
+    case "+":
+      return add(a, b);
+      break;
+    case "-":
+      return subtract(a, b);
+      break;
+    case "*":
+      return multiply(a, b);
+      break;
+    case "/":
+      return divide(a, b);
+      break;
+    default:
+      return;
+      break;
+  }
 }
