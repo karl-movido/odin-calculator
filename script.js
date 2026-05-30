c13a07760123145e0c5f11214af6aa3d6f180bfd
Karl Movido <karl.movido@gmail.com>
2026-05-30 11:38:33 +0800

    feat: added calculate function

--- script.js
+++ script.js
@@ -14,9 +14,13 @@ function divide(a, b) {
   return a / b;
 }
 
-const num1 = 0;
-const num2 = 0;
-const op = "";
+function modulus(a, b) {
+  return a % b;
+}
+
+let num1 = "";
+let num2 = "";
+let op = "";
 
 function operate(a, b, op) {
   switch (op) {
@@ -26,14 +30,67 @@ function operate(a, b, op) {
     case "-":
       return subtract(a, b);
       break;
-    case "*":
+    case "x":
       return multiply(a, b);
       break;
     case "/":
       return divide(a, b);
       break;
+    case "%":
+      return modulus(a, b);
+      break;
     default:
       return;
       break;
   }
+}
+
+let result = 0;
+
+const controls = document.getElementById("controls");
+
+let previousDisplay = "";
+
+controls.addEventListener("click", (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const value = e.target.dataset.value;
+
+    if (!isNaN(value)) {
+      if (!op) {
+        num1 += value;
+        updateDisplay(num1);
+      } else {
+        num2 += value;
+        updateDisplay(num2);
+      }
+    }
+
+    if (["+", "-", "/", "%", "x"].includes(value)) {
+      if (!op) {
+        op = value;
+      } else {
+        const result = operate(Number(num1), Number(num2), op);
+
+        updateDisplay(result);
+
+        num1 = result.toString();
+        num2 = "";
+        op = value;
+      }
+    }
+
+    if (value === "=") {
+      const result = operate(Number(num1), Number(num2), op);
+
+      updateDisplay(result);
+
+      num1 = result.toString();
+      num2 = "";
+      op = "";
+    }
+  }
+});
+
+function updateDisplay(value) {
+  document.getElementById("result").innerHTML = value;
 }
